daf2caa0310a11e68ca281807ef8dcd3fad9009c
shiva foroughan <shiva.foroughan@outlook.com>
2023-10-04 10:05:38 +0330

    add morgan and create DB models

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const { AllRoutes } = require('./router/router')
+const morgan = require('morgan')
 
 class Application {
   #app = express()
@@ -19,6 +20,7 @@ class Application {
   }
 
   configApplication() {
+    this.#app.use(morgan('dev'))
     this.#app.use(express.json())
     this.#app.use(express.urlencoded({ extended: true }))
     this.#app.use(express.static(path.join(__dirname, '..', 'public')))
@@ -38,8 +40,19 @@ class Application {
       .connect(this.#DB_URl)
       .then(() => console.log('server connected to mongodb'))
       .catch((err) => {
-        throw err
+        throw err.message
       })
+    mongoose.connection.on('connected', () => {
+      console.log('mongoose connected to DB')
+    })
+    mongoose.connection.on('disconnected', () => {
+      console.log('mongoose connection has been lost!')
+    })
+    process.on('SIGINT', async () => {
+      console.log('Hello i recived SIGINT')
+      await mongoose.connection.close()
+      console.log('MongoDB Disconnected')
+    })
   }
 
   createRoutes() {
